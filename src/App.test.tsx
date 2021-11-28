bc1acb597aba10c62e0a37fb44023123f373e054
Szabolcs Forreiter <forreiter.szabolcs@johnlewis.co.uk>
2021-11-28 15:11:11 +0200

    ran out of time

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,33 @@ import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import App from './App'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  )
+describe('app.txs', () => {
+  test('renders app', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
 
-  expect(getByText(/title/i)).toBeInTheDocument()
+    expect(getByText(/title/i)).toBeInTheDocument()
+  })
+
+  test('should show spinner and error after call fails', () => {
+    jest.spyOn(global, 'fetch').mockRejectedValueOnce(new Error('Async error'))
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+
+    const button = getByText('get launches')
+    userEvent.click(button)
+    const spinner = getByText('spinner')
+    expect(spinner).toBeInTheDocument()
+
+    // await waitForElementToBeRemoved(spinner) TODO: find this method's import location :D
+  })
 })
